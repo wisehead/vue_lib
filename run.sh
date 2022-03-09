fd668576d0de2905818b57c18d271c11cf8825e5
wisehead <alex.chenhui@gmail.com>
2022-03-09 19:54:33 +0800

    install

---
+++ run.sh
@@ -0,0 +1,9 @@
+#############################################################
+#   File Name: run.sh
+#     Autohor: Hui Chen (c) 2022
+#        Mail: alex.chenhui@gmail.com
+# Create Time: 2022/03/09-19:54:10
+#############################################################
+#!/bin/sh 
+#install webpack
+cnpm install webpack -g
