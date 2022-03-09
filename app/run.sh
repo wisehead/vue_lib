51e95b86a21084f2b46a902e3ac8c33113c4bcc8
wisehead <alex.chenhui@gmail.com>
2022-03-09 20:28:47 +0800

    webpack ok

---
+++ app/run.sh
@@ -0,0 +1,8 @@
+#############################################################
+#   File Name: run.sh
+#     Autohor: Hui Chen (c) 2022
+#        Mail: alex.chenhui@gmail.com
+# Create Time: 2022/03/09-20:21:32
+#############################################################
+#!/bin/sh 
+webpack runoob1.js bundle.js
