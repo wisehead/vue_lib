841b6216ef55a0bd7e18dbcad5e83841392c7acb
wisehead <alex.chenhui@gmail.com>
2022-03-10 09:35:43 +0800

    style loader works, turns yellow

---
+++ app/dist/run.sh
@@ -0,0 +1,8 @@
+#############################################################
+#   File Name: run.sh
+#     Autohor: Hui Chen (c) 2022
+#        Mail: alex.chenhui@gmail.com
+# Create Time: 2022/03/10-09:34:59
+#############################################################
+#!/bin/sh 
+cp main.js ../bundle.js
