f0ac78513bf4bb7af08a5b8e6feb76943b544c1b
wisehead <alex.chenhui@gmail.com>
2022-03-13 21:16:10 +0800

    create

---
+++ 03.create_app/run.sh
@@ -0,0 +1,10 @@
+#############################################################
+#   File Name: run.sh
+#     Autohor: Hui Chen (c) 2022
+#        Mail: alex.chenhui@gmail.com
+# Create Time: 2022/03/13-21:15:47
+#############################################################
+#!/bin/sh 
+vue create runoob-vue3-app
+cd runoob-vue3-app
+npm run serve
