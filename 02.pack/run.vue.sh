638f14402a4ed5b0c0cdcf9b8df30b9bb78e89fa
wisehead <alex.chenhui@gmail.com>
2022-03-13 21:07:19 +0800

    cnpm run build

---
+++ 02.pack/run.vue.sh
@@ -0,0 +1,10 @@
+#############################################################
+#   File Name: run.sh
+#     Autohor: Hui Chen (c) 2022
+#        Mail: alex.chenhui@gmail.com
+# Create Time: 2022/03/13-20:58:29
+#############################################################
+#!/bin/sh 
+vue init webpack runoob-vue3-test
+cd runoob-vue3-test
+cnpm run dev
---
+++ 02.pack/runoob-vue3-test/run_pack.sh
@@ -0,0 +1,8 @@
+#############################################################
+#   File Name: run_pack.sh
+#     Autohor: Hui Chen (c) 2022
+#        Mail: alex.chenhui@gmail.com
+# Create Time: 2022/03/13-21:07:03
+#############################################################
+#!/bin/sh 
+cnpm run build
