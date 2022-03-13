ee373c8c987396c90bd3a7b6d334d2dfc568a3b7
wisehead <alex.chenhui@gmail.com>
2022-03-13 21:04:13 +0800

    command of vite

---
+++ 01.hello/run.vite.sh
@@ -0,0 +1,11 @@
+#############################################################
+#   File Name: run.vite.sh
+#     Autohor: Hui Chen (c) 2022
+#        Mail: alex.chenhui@gmail.com
+# Create Time: 2022/03/13-21:03:44
+#############################################################
+#!/bin/sh 
+cnpm init @vitejs/app runoob-vue3-test2
+cd runoob-vue3-test2
+cnpm install
+cnpm run dev
